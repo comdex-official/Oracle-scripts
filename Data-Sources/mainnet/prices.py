13ffab63f4e50f12a5f5a6733696e65271ccba1d
Vishnu Kumavat <vishnukumawat0950@gmail.com>
2023-03-11 14:19:19 +0530

    mainnet oracle script update

--- Data-Sources/mainnet/prices.py
+++ Data-Sources/mainnet/prices.py
@@ -4,85 +4,36 @@ import requests
 import json
 import sys
 
+STAT_URL = "https://stat.comdex.one"
+
 CONSTANTS = {
     "OSMOSIS": {
         "SYMBOLS": {
-            "ATOM": "ATOM",
-            "OSMO": "OSMO",
-            "SCRT": "SCRT",
-            "AKT": "AKT",
-            "JUNO": "JUNO",
-            "CRO": "CRO",
-            "ION": "ION",
-            "XPRT": "XPRT",
-            "DVPN": "DVPN",
-            "REGEN": "REGEN",
-            "NGM": "NGM",
-            "EEUR": "EEUR",
-            "BAND": "BAND",
-            "CMDX": "CMDX",
-            "STARS": "STARS",
-            "HUAHUA": "HUAHUA",
-            "DSM": "DSM",
-            "AXLWBTC": "WBTC",
-            "WETH": "WETH",
-            "AXLUSDC": "USDC",
-            "AXLUSDT": "USDT",
-            "AXLDAI": "DAI",
-            "GWETH": "WETH.grv",
-            "GUSDC": "USDC.grv",
-            "GUSDT": "USDT.grv",
-            "WMATIC": "WMATIC",
-            "AXLWBNB": "WBNB",
-            "EVMOS": "EVMOS",
-            "STATOM": "stATOM",
-            "STOSMO": "stOSMO",
-            "LUNA": "LUNA",
-            "MNTL": "MNTL",
-            "AXLWFTM": "WFTM"
+            "ATOM": "ATOM", "OSMO": "OSMO", "SCRT": "SCRT", "AKT": "AKT", "JUNO": "JUNO", "CRO": "CRO", "ION": "ION",
+            "XPRT": "XPRT", "DVPN": "DVPN", "REGEN": "REGEN", "NGM": "NGM", "EEUR": "EEUR", "BAND": "BAND", "CMDX": "CMDX",
+            "STARS": "STARS", "HUAHUA": "HUAHUA", "DSM": "DSM", "AXLWBTC": "WBTC", "AXLWETH": "WETH", "AXLUSDC": "USDC",
+            "AXLUSDT": "USDT", "AXLDAI": "DAI", "GWETH": "WETH.grv", "GRVUSDC": "USDC.grv", "GUSDT": "USDT.grv", "WMATIC": "WMATIC",
+            "AXLWBNB": "WBNB", "EVMOS": "EVMOS", "STATOM": "stATOM", "STOSMO": "stOSMO", "LUNA": "LUNA", "CMST": "CMST",
         },
         "URL": "https://api-osmosis.imperator.co/tokens/v2/all"
     },
     "COIN_GECKO": {
         "SYMBOLS": {
-            "ATOM": "cosmos",
-            "OSMO": "osmosis",
-            "SCRT": "secret",
-            "AKT": "akash-network",
-            "JUNO": "juno-network",
-            "CRO": "crypto-com-chain",
-            "ION": "ion",
-            "XPRT": "persistence",
-            "DVPN": "sentinel",
-            "REGEN": "regen",
-            "NGM": "e-money",
-            "EEUR": "e-money-eur",
-            "BAND": "band-protocol",
-            "CMDX": "comdex",
-            "STARS": "stargaze",
-            "HUAHUA": "chihuahua-token",
-            "DSM": "desmos",
-            "AXLWBTC": "axlwbtc",
-            "WETH": "axlweth",
-            "AXLUSDC": "axlusdc",
-            "AXLDAI": "dai",
-            "GWBTC": "gravity-bridge-wbtc",
-            "GWETH": "gravity-bridge-weth",
-            "GUSDC": "gravity-bridge-usdc",
-            "GUSDT": "gravity-bridge-tether",
-            "GDAI": "gravity-bridge-dai",
-            "WMATIC": "wmatic",
-            "AXLWBNB": "wbnb",
-            "EVMOS": "evmos",
-            "STATOM": "stride-staked-atom",
-            "STOSMO": "stride-staked-osmo",
-            "LUNA": "terra-luna-2",
-            "CANTO": "canto",
-            "MNTL": "assetmantle",
-            "AXLWFTM": "wrapped-fantom",
+            "ATOM": "cosmos", "OSMO": "osmosis", "SCRT": "secret", "AKT": "akash-network", "JUNO": "juno-network", "CRO": "crypto-com-chain", 
+            "ION": "ion", "XPRT": "persistence", "DVPN": "sentinel", "REGEN": "regen", "NGM": "e-money", "EEUR": "e-money-eur", "BAND": "band-protocol",
+            "CMDX": "comdex", "STARS": "stargaze", "HUAHUA": "chihuahua-token", "DSM": "desmos", "AXLWBTC": "axlwbtc", "AXLWETH": "axlweth",
+            "AXLUSDC": "axlusdc", "AXLDAI": "dai", "GWBTC": "gravity-bridge-wbtc", "GWETH": "gravity-bridge-weth", "GUSDC": "gravity-bridge-usdc", 
+            "GUSDT": "gravity-bridge-tether", "GDAI": "gravity-bridge-dai", "WMATIC": "wmatic", "AXLWBNB": "wbnb", "EVMOS": "evmos",
+            "STATOM": "stride-staked-atom", "STOSMO": "stride-staked-osmo", "LUNA": "terra-luna-2", "CANTO": "canto", "CMST": "composite"
         },
         "URL": "https://api.coingecko.com/api/v3/simple/price"
     },
+    "CSWAP": {
+        "SYMBOLS": {
+            "CMST": "CMST", "HARBOR": "HARBOR"
+        },
+        "URL": f"{STAT_URL}/api/v2/cswap/tokens/all"
+    }
 }
 
 
@@ -154,17 +105,37 @@ def get_price_osmosis(symbols):
         return [0 for i in range(len(symbols))]
 
 
-def get_price_cswap(symbol):
-    URL = f"https://stat.comdex.one/api/v2/cswap/tokens/{symbol}"
+def get_price_cswap(symbols):
+    # Return empty response if no symbols
+    if not symbols:
+        return []
+    
+    # Set a header for api
     HEADER = {
         "Accept": "application/json",
         "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:105.0) Gecko/20100101 Firefox/105.0"
     }
+
+    # URL to retrieve the price of all tokens
+    url = CONSTANTS["CSWAP"]["URL"]
+
+    # Request prices of all tokens from API and retrieve the prices for
+    # requested tokens. Return 0, if the request fails.
     try:
-        result = requests.get(URL, headers=HEADER).json()
-        return result["data"]['price'] if isinstance(result["data"]['price'], float) else 0
+        response = requests.get(url, headers=HEADER).json()
+        
+        # return all prices as 0 if API fails or returns error
+        if response.get("error"):
+            return [0 for i in range(len(symbols))]
+        
+        cswap_symbol_prices_map = {}
+        for token in response["data"]:
+            cswap_symbol_prices_map[token["symbol"]] = token["price"]
+
+        return [cswap_symbol_prices_map[CONSTANTS["CSWAP"]["SYMBOLS"][symbol]] if CONSTANTS["CSWAP"]["SYMBOLS"].get(symbol, None) else 0  for symbol in symbols]
+    
     except Exception as e:
-        return 0
+        return [0 for i in range(len(symbols))]
 
 
 def main(symbols):
@@ -172,43 +143,33 @@ def main(symbols):
         return ""
 
     try:
-        # Get CMST price
-        cmst_index = None
-        if "CMST" in symbols:
-            cmst_index = symbols.index("CMST")
-            symbols.pop(cmst_index)
-            cmst_price = get_price_cswap("CMST")
-
-        # Get HARBOR price
-        harbor_index = None
-        if "HARBOR" in symbols:
-            harbor_index = symbols.index("HARBOR")
-            symbols.pop(harbor_index)
-            harbor_price = get_price_cswap("HARBOR")
-
         # Get price from coin gecko
         result_coingecko = get_price_coingecko(symbols)
+
         # Get price from osmosis
         result_osmosis = get_price_osmosis(symbols)
 
+        # Get price from cswap
+        result_cswap = get_price_cswap(symbols)
+
+        # if lenghth of the results from all the sources is not same, then return 0
+        if not len(result_coingecko) == len(result_osmosis) == len(result_cswap):
+            return ",".join("0" for i in range(len(symbols)))
+
         result = []
-        for item in zip(result_coingecko, result_osmosis):
-            if item[0] == 0 and item[1] != 0:
-                result.append(item[1])
-            elif item[0] != 0 and item[1] == 0:
-                result.append(item[0])
+        for item in zip(result_coingecko, result_osmosis, result_cswap):
+            different_sources_price = list(item)
+            print(different_sources_price)
+            non_zero_sources = [price for price in different_sources_price if price != 0]
+            if non_zero_sources:
+                mean = sum(non_zero_sources) / len(non_zero_sources)
+                result.append(mean)
             else:
-                result.append(sum(item)/2)
+                result.append(0)
 
-        # Insert cmst price in same order as requested symbols
-        if harbor_index is not None:
-            result.insert(harbor_index, harbor_price)
-        if cmst_index is not None:
-            result.insert(cmst_index, cmst_price)
         return ",".join(str(price) for price in result)
-    except Exception:
+    except Exception as e:
         return ",".join("0" for i in range(len(symbols)))
-
 
 if __name__ == "__main__":
     try:
